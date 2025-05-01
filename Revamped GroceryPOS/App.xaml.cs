f8def6f0c15d4157203687cf237e1e08a582e4cd
Kenneth Reniel Viovicente <151752526+kennethviov@users.noreply.github.com>
2025-05-02 00:37:06 +0800

    Add project files.

---
+++ Revamped GroceryPOS/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Revamped_GroceryPOS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
